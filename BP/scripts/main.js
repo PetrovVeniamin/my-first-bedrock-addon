6c231ad9b2faae1bda42759c657be8d48730cd14
PetrovVeniamin <petrovviniamin495@gmail.com>
2026-09-18 20:50:24 +0200

    new mainjs

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -1,2 +1,10 @@
 import {world}  from "@minecraft/server";
 
+world.afterEvents.playerBreakBlock.subscribe((eventData)) => {
+
+ const player = eventData.player;
+
+ const inventory = player.getComponent("minecraft:inventory").container;
+
+ 
+}
